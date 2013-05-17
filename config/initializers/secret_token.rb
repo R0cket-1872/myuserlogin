74cdad193a86f10559d74e583cec5de8dac95489
Mark Sherer <m.v.sherer.07@aberdeen.ac.uk>
2013-05-17 17:30:31 +0100

    First commit, bundled initial gems and home controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Profile::Application.config.secret_token = '69b24cfdf585468acef5c704bf906c850363c8a97bdcf158e20a9a2f42d894346fdef5f6d8a15da48152fae1bbac19fda4655f609bb274037b3a2a7369c54015'
